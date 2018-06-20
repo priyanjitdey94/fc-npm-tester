4376cb2bd804c334fe85b3df460fe8b3a4e8c250
Rohit Kumar <rohit.kr001@gmail.com>
2018-06-20 17:17:26 +0530

    Add babel configuration in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-// import json from 'rollup-plugin-json';
+import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 // import uglify from 'rollup-plugin-uglify';
 
@@ -9,16 +9,29 @@ export default {
   input: 'src/renderer.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'iife'
+    format: 'cjs'
   },
   inlineDynamicImports: true,
   experimentalDynamicImport: true,
   experimentalCodeSplitting: true,
   plugins: [
-    // json(),
+    json(),
     resolve(),
     commonjs(),
-    babel(),
+    babel({
+      'presets': [
+        [
+          'env',
+          {
+            'modules': false
+          }
+        ]
+      ],
+      'plugins': [
+        'syntax-dynamic-import',
+        'external-helpers'
+      ]
+    }),
     // uglify()
   ]
 };
